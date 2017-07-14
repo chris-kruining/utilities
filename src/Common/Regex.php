e58ec37db8dcdae968b3a0e3a835cf8a4f7b42e3
Chris Kruining <c.kruining@fyn.nl>
2017-07-14 21:41:49 +0200

    moved functions from vivid's regex class to this regex class

--- src/Common/Regex.php
+++ src/Common/Regex.php
@@ -10,5 +10,25 @@ namespace CPB\Utilities\Common
 
             return $matches;
         }
+    
+        public static function Encapsulate(string $input, string $char, string $filter = null, string $lookFor = 'A-Za-z0-9_', int $limit = -1) : string
+        {
+            $filter = $filter ?? $char;
+        
+            return preg_replace(
+                '/(?![^' . $filter . ']*[' . $filter . '](?:[^' . $filter . ']*[' . $filter . '][^' . $filter . ']*[' . $filter . '])*[^' . $filter . ']*$)([' . $lookFor . ']+)/',
+                $char . '$1' . $char,
+                $input,
+                $limit
+            );
+        }
+    
+        public static function AfterLastOccurrence(string $subject, string $character = '/') : string
+        {
+            preg_match('/.*\\' . $character . '(.*)/', $subject, $match);
+        
+            return $match[1];
+        }
+    
     }
 }
