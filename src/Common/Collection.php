9f2d97fd133e6f77d16c2d26ddf28d97a577306e
Chris <c.kruining@fyn.nl>
2017-07-31 15:00:29 +0200

    'expanded' the default PHP definition of what is a callable

--- src/Common/Collection.php
+++ src/Common/Collection.php
@@ -2,6 +2,9 @@
 
 namespace CPB\Utilities\Common
 {
+    use CPB\Utilities\Code\Lambda;
+    use CPB\Utilities\Code\UnparsableException;
+
     class Collection implements \Countable, \IteratorAggregate, \ArrayAccess, \Serializable, \JsonSerializable
     {
         protected $items;
@@ -14,20 +17,30 @@ namespace CPB\Utilities\Common
         public function __call($method, $parameters)
         {
             $function = 'array' . preg_replace_callback(
-                '/[A-Z]/',
-                function($match) {
-                    return '_' . strtolower($match[0]);
-                },
-                $method
-            );
+                    '/[A-Z]/',
+                    function($match) {
+                        return '_' . strtolower($match[0]);
+                    },
+                    $method
+                );
 
-			// TODO(Chris Kruining) 
-			// Find a way to reference the 
-			// collection instance from a 
-			// callable without changing the 
-			// $this argument as this is 
-			// considered bad practice
-			
+            // TODO(Chris Kruining)
+            // Find a way to reference the
+            // collection instance from a
+            // callable without changing the
+            // $this argument as this is
+            // considered bad practice
+            array_map(function($parameter){
+                try
+                {
+                    return static::GetCallable($parameter);
+                }
+                catch(\InvalidArgumentException $e)
+                {
+                    return $parameter;
+                }
+            }, $parameters);
+
             if(function_exists($function))
             {
                 $result = $function($this->items, ...$parameters);
@@ -51,32 +64,36 @@ namespace CPB\Utilities\Common
         {
             return Collection::From($this->items);
         }
-		
-		public function __debugInfo() : array
-		{
-			return $this->items;
-		}
 
-        public function Map(callable $callback): Collection
+        public function __debugInfo() : array
         {
-            return static::From(
-				array_map(
-					$callback, 
-					array_keys($this->items), 
-					array_values($this->items)
-				)
-			);
+            return $this->items;
         }
 
-		// NOTE(Chris Kruining)
-		// This function is meant to 
-		// take an action on each item 
-		// in the collection allowing 
-		// to change key and value, 
-		// whereas Map only allows for 
-		// changes in the value
-        public function Each(callable $callback): Collection
+        public function Map($callback): Collection
         {
+            $callback = static::GetCallable($callback);
+
+            return static::From(
+                array_map(
+                    $callback,
+                    array_keys($this->items),
+                    array_values($this->items)
+                )
+            );
+        }
+
+        // NOTE(Chris Kruining)
+        // This function is meant to
+        // take an action on each item
+        // in the collection allowing
+        // to change key and value,
+        // whereas Map only allows for
+        // changes in the value
+        public function Each($callback): Collection
+        {
+            $callback = static::GetCallable($callback);
+
             $collection = new static;
 
             foreach($this->items as $key => $value)
@@ -85,7 +102,7 @@ namespace CPB\Utilities\Common
 
                 // TODO(Chris Kruining)
                 // Implement more methods of
-                // transfering key => value pairs
+                // transferring key => value pairs
                 switch(gettype($result))
                 {
                     case 'object':
@@ -115,7 +132,7 @@ namespace CPB\Utilities\Common
 
             return $collection->count() === 0 ? $this : $collection;
         }
-		
+
         // courtesy of https://stackoverflow.com/a/6092999
         public function PowerSet(int $minLength = 1) : Collection
         {
@@ -152,6 +169,20 @@ namespace CPB\Utilities\Common
             $inst->items = $items;
 
             return $inst;
+        }
+
+        protected static function GetCallable($callable) : callable
+        {
+            if(!is_callable($callable) && !is_string($callable))
+            {
+                throw new \InvalidArgumentException(
+                    '$callback is not a valid parameter'
+                );
+            }
+
+            return !is_callable($callable) && is_string($callable)
+                ? Lambda::From($callable)
+                : $callable;
         }
 
         public function ToArray()
