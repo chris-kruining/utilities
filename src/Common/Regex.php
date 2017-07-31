04954e5b27450a1de42d79bb36c563d57edaa917
Chris <c.kruining@fyn.nl>
2017-07-31 14:36:11 +0200

    split off Match into Match and MatchAll so I no longer hide php functionality

--- src/Common/Regex.php
+++ src/Common/Regex.php
@@ -6,15 +6,22 @@ namespace CPB\Utilities\Common
     {
         public static function Match(string $pattern, string $subject) : array
         {
+            preg_match($pattern, $subject, $matches);
+
+            return $matches;
+        }
+
+        public static function MatchAll(string $pattern, string $subject) : array
+        {
             preg_match_all($pattern, $subject, $matches);
 
             return $matches;
         }
-    
+
         public static function Encapsulate(string $input, string $char, string $filter = null, string $lookFor = 'A-Za-z0-9_', int $limit = -1) : string
         {
             $filter = $filter ?? $char;
-        
+
             return preg_replace(
                 '/(?![^' . $filter . ']*[' . $filter . '](?:[^' . $filter . ']*[' . $filter . '][^' . $filter . ']*[' . $filter . '])*[^' . $filter . ']*$)([' . $lookFor . ']+)/',
                 $char . '$1' . $char,
@@ -22,10 +29,10 @@ namespace CPB\Utilities\Common
                 $limit
             );
         }
-    
+
         public static function AfterLastOccurrence(string $subject, string $character = '/') : string
         {
-            return static::Match('/.*\\' . $character . '(.*)/', $subject)[1][0];
+            return static::MatchAll('/.*\\' . $character . '(.*)/', $subject)[1][0];
         }
     }
 }
