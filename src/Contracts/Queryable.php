256e0723a39a9ee727a34f42cdcb5a87a396d9c6
Chris <c.kruining@fyn.nl>
2017-09-14 12:31:11 +0200

    updated the Queryable contract

--- src/Contracts/Queryable.php
+++ src/Contracts/Queryable.php
@@ -13,19 +13,36 @@ namespace CPB\Utilities\Contracts
         const JOIN_RIGHT = 3;
 
         public function select($key): Queryable;
+
         public function where(): Queryable;
-        public function join(iterable $iterable, string $localKey, string $foreignKey, int $strategy = self::JOIN_INNER): Queryable;
+
         public function limit(int $length): Queryable;
+
         public function offset(int $start): Queryable;
+
+        public function join(
+            iterable $iterable,
+            string $localKey,
+            string $foreignKey,
+            int $strategy = self::JOIN_INNER
+        ): Queryable;
+
         public function union(iterable $iterable): Queryable;
+
         public function distinct(string $key): Queryable;
+
         public function order(string $key, int $direction): Queryable;
+
         public function group(string $key): Queryable;
+
         public function sum(string $key = null);
+
         public function average(string $key);
+
         public function max(string $key, float $limit);
+
         public function min(string $key, float $limit);
+
         public function clamp(string $key, float $lower, float $upper);
-        public function contains($value): bool;
     }
 }
