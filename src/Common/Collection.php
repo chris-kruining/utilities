2cb8b4c4deee2919c4de89b8505405e10eb3ecdc
Chris <c.kruining@fyn.nl>
2017-10-10 18:41:41 +0200

    dafeauq

--- src/Common/Collection.php
+++ src/Common/Collection.php
@@ -585,7 +585,7 @@ namespace CPB\Utilities\Common
                                 $row = $row[$key] ?? ($row instanceof CollectionInterface
                                     ? $row->map(function($k, $v) use($key){ return $v[$key] ?? null; })->filter()
                                     : array_filter(array_map(function($v) use($key){ return $v[$key] ?? null; }, $row))
-                                );
+                                ) ?? null;
                             }
                         }
 
