06179d779cbe2d5333477c9cfcd328d7b59101dd
Chris <c.kruining@fyn.nl>
2017-10-17 17:43:12 +0200

    omited the default values as this should be decided by the class using the enum

--- src/Enums/JoinStrategy.php
+++ src/Enums/JoinStrategy.php
@@ -5,7 +5,6 @@ namespace CPB\Utilities\Enums
     class JoinStrategy
     {
         public const
-            __default = self::INNER,
             INNER = 0,
             OUTER = 1,
             LEFT = 2,
--- src/Enums/SortDirection.php
+++ src/Enums/SortDirection.php
@@ -5,7 +5,6 @@ namespace CPB\Utilities\Enums
     class SortDirection extends \SplEnum
     {
         public const
-            __default = self::ASC,
             ASC = 0,
             DESC = 1
         ;
