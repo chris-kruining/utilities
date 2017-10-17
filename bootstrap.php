98c5bce88bdf25860b80cb4ee5561de1093736aa
Chris <c.kruining@fyn.nl>
2017-10-17 17:41:23 +0200

    shift to json encoding to get a better score

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 error_reporting(E_ALL);
 
 set_error_handler(function($errno, $errstr, $errfile, $errline){
-    print_r([
+    echo json_encode([
         'error' => $errno,
         'message' => $errstr,
         'file' => $errfile,
