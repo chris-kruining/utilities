82056dc598d579de8f48e9e16feffce15aa7e0cd
Chris P Bacon <chris@kruining.eu>
2017-11-08 08:53:30 +0100

    started adding serialization

--- src/Math/Matrix.php
+++ src/Math/Matrix.php
@@ -4,19 +4,41 @@ namespace CPB\Utilities\Math
 {
     use CPB\Utilities\Common\Collection;
 
-    class Matrix
+    class Matrix implements \JsonSerializable
     {
         protected
             $data,
             $width,
-            $height
-        ;
+            $height;
 
         public function __construct(int $width, int $height)
         {
             $this->data = new Collection;
             $this->width = $width;
             $this->height = $height;
+        }
+
+        public function __toString(): string
+        {
+            return $this->data
+                ->map(function($k, $v)
+                {
+                    return $v->toString(',');
+                })
+                ->toString(',');
+        }
+
+        public function toArray(): array
+        {
+            return $this->data->reduce(function($t, $k, $v)
+            {
+                return array_merge($t, $v->toArray());
+            });
+        }
+
+        public function jsonSerialize(): array
+        {
+            return $this->toArray();
         }
 
         public static function from(iterable $src, int $width, int $height): Matrix
