f4cbae428060bed8881cba6b4bf45fbbd7281c6c
Chris <c.kruining@fyn.nl>
2018-01-10 18:58:25 +0100

    added Macroable decorator

---
+++ src/Common/Exceptions/MethodNotAvailable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CPB\Utilities\Common\Exceptions
+{
+    class MethodNotAvailable extends \BadMethodCallException
+    {
+        public function __construct(string $class, string $method)
+        {
+            $trace = $this->getTrace();
+            
+            parent::__construct(\sprintf(
+                '\'%s::%s\' is neither a macro\'ed method nor accessible or does not exist',
+                $class,
+                $method
+            ));
+            
+            $this->file = $trace[0]['file'];
+            $this->line = $trace[0]['line'];
+        }
+    }
+}
---
+++ src/Decorations/Marcoable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CPB\Utilities\Decorations
+{
+    use CPB\Utilities\Collections\Collection;
+    use CPB\Utilities\Common\Exceptions\MethodNotAvailable;
+    
+    trait Marcoable
+    {
+        private static
+            $classes = null
+        ;
+        
+        public function __construct()
+        {
+            if(self::$classes === null)
+            {
+                self::$classes = new Collection;
+            }
+            
+            $class = self::class;
+            $methods = Collection::from((new \ReflectionClass($class))->getMethods())
+                ->filter(function($m) { return \strpos($m->getDocComment() ?: '', '@macro') !== false; })
+                ->map(function($k, $v){ return $v->name; });
+    
+            self::$classes[$class] = $methods;
+        }
+        
+        public function __call(string $name, array $arguments)
+        {
+            if(!self::mayCall($name))
+            {
+                throw new MethodNotAvailable(self::class, $name);
+            }
+            
+            return $this->$name(...$arguments);
+        }
+        
+        public static function __callStatic(string $name, array $arguments)
+        {
+            // NOTE(Chris Kruining)
+            // Creating a new instance without
+            // conditions seems counter intuitive
+            // but makes sure the called class
+            // get a chance to register itself
+            return (new static())->__call($name, $arguments);
+        }
+        
+        private static function mayCall(string $method): bool
+        {
+            return self::$classes->has(self::class) && self::$classes[self::class]->includes($method);
+        }
+    }
+}
