f63bf6b00a3910f3d1d6a2aa475fc090b9dfa683
Chris <c.kruining@fyn.nl>
2018-02-07 19:58:23 +0100

    made a little start on a DateRange object, this would be one layer up from DatePeriod to have basically a DatePeriod with holes in it

---
+++ src/Common/DateRange.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CPB\Utilities\Common
+{
+    class DateRange implements \IteratorAggregate
+    {
+        protected
+            $periods;
+    
+        public function __construct(\DatePeriod ...$periods)
+        {
+            $this->periods = \CPB\Utilities\Collections\Collection::from($periods);
+        }
+        
+        public function add(\DatePeriod $period, \DatePeriod ...$periods): DateRange
+        {
+            \array_unshift($periods, $period);
+            
+            foreach($periods as $period)
+            {
+                \var_dump($this->periods->filter(function(\DatePeriod $p) use($period){ $period->getEndDate() >= $p->getStartDate() || $period->getStartDate() <= $p->getEndDate(); }));
+            }
+            
+            die;
+            
+            return $this;
+        }
+    
+        public function getIterator()
+        {
+            foreach($this->periods as $period)
+            {
+                foreach($period as $dateTime)
+                {
+                    yield $dateTime;
+                }
+            }
+        }
+    }
+}
