6322aa96a78d4a4df8e6f819f6b613b6dd098707
Chris Kruining <c.kruining@fyn.nl>
2018-10-05 12:25:19 +0200

    expanded the performance tool a great deal

--- src/Tools/Performance.php
+++ src/Tools/Performance.php
@@ -2,9 +2,14 @@
 
 namespace CPB\Utilities\Tools
 {
+    use CPB\Utilities\Collections\Collection;
+    
     final class Performance
     {
-        private $start = 0;
+        private
+            $start = 0,
+            $marks = []
+        ;
         
         private function __construct()
         {
@@ -18,9 +23,43 @@ namespace CPB\Utilities\Tools
             return $inst;
         }
         
-        public function stop(): void
+        public function mark(int $offset = 0): void
         {
-            echo \sprintf('<p>%sms</p>', (float)number_format(microtime(true) - $this->start, 10) * 1000);
+            $this->marks[] = [
+                microtime(true),
+                debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $offset + 1)[$offset] ?? []
+            ];
+        }
+        
+        public function stop(int $offset = 1): void
+        {
+            $this->mark($offset);
+            $last = $this->start;
+            
+            foreach($this->marks as [ $mark, $debug ])
+            {
+                echo \sprintf(
+                    '<p>%sms - %s::%s</p>',
+                    (float)number_format($mark - $last, 10) * 1000,
+                    $debug['file'],
+                    $debug['line']
+                );
+                
+                $last = $mark;
+            }
+        }
+        
+        public function any(callable $callback): bool
+        {
+            $l = $this->start;
+            
+            return Collection::from($this->marks)
+                ->map(function($k, $v) use(&$l){
+                    $r = (float)number_format($v[0] - $l, 10) * 1000;
+                    $l = $v[0];
+                    return $r;
+                })
+                ->some(function($k, $v) use($callback){ return $callback($v); });
         }
         
         public static function measure(callable $cb, int ...$repeat)
@@ -36,10 +75,10 @@ namespace CPB\Utilities\Tools
     
                 for($i = 0; $i < $iterations; $i++)
                 {
-                    $result = $cb($i);
+                    $result = $cb($i, $inst);
                 }
     
-                $inst->stop();
+                $inst->stop(2);
             }
             
             return $result;
