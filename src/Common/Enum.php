a4f95e7851dc96c2ec7d7de83072c73c3170a1df
Chris Kruining <c.kruining@fyn.nl>
2019-12-06 12:17:33 +0100

    added 'getKey' to enums

--- src/Common/Enum.php
+++ src/Common/Enum.php
@@ -7,43 +7,48 @@ namespace CPB\Utilities\Common
         private
             $value
         ;
-        
+
         private static
             $map = []
         ;
-        
+
         public final function __construct($value = null)
         {
             $this->value = $value;
         }
-        
+
         public final function __toString(): string
         {
             return (string)$this->value;
         }
-    
+
+        public final function getkey(): string
+        {
+            return \array_flip(self::$map[static::class])[$this->value];
+        }
+
         public final function getValue()
         {
             return $this->value;
         }
-        
+
         public final function jsonSerialize()
         {
             $constants = self::$map[static::class];
-            
+
             return \sprintf('%s::%s', static::class, \array_flip($constants)[$this->value]);
         }
-    
+
         public final static function from($key)
         {
             if(!\key_exists(static::class, self::$map))
             {
                 self::$map[static::class] = (new \ReflectionClass(static::class))->getConstants();
             }
-            
+
             $constants = self::$map[static::class];
-            
-            
+
+
             if(!\key_exists($key, $constants) && \array_search($key, $constants) === false)
             {
                 throw new \InvalidArgumentException(\sprintf(
@@ -52,11 +57,11 @@ namespace CPB\Utilities\Common
                     static::class
                 ));
             }
-            
+
             $value = \key_exists($key, $constants)
                 ? $constants[$key]
                 : $key;
-            
+
             return new static($value);
         }
     }
