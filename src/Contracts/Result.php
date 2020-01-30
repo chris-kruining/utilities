1ceacbd6ae9c488587e4d954c33783015e5397cd
Chris Kruining <c.kruining@fyn.nl>
2020-01-30 09:13:52 +0100

    fixed small issue

--- src/Contracts/Result.php
+++ src/Contracts/Result.php
@@ -4,6 +4,8 @@ namespace CPB\Utilities\Contracts
 {
     interface Result extends \Countable, \JsonSerializable
     {
+        public function __invoke(): bool;
+
         public function getResult(): bool;
         public function getErrors(): array;
     }
